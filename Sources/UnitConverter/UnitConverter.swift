e131421a1bb570822addb7a7ffadc2d30e0908ff
Cody Morley <codymorleyinc@gmail.com>
2020-08-26 10:32:26 -0700

    hotfix

--- Sources/UnitConverter/UnitConverter.swift
+++ Sources/UnitConverter/UnitConverter.swift
@@ -1,10 +1,10 @@
-public struct UnitConverter {
+public class UnitConverter {
     /// Just because the struct is public doesn't mean the things in struct are too.
     /// You must mark public anything you want to be externally accessible.
     
     private let secretNumber = 42
     
-    func poundsToKilograms(_ pounds: Double) -> Double {
+    public func poundsToKilograms(_ pounds: Double) -> Double {
         pounds * 2.2
     }
 }
